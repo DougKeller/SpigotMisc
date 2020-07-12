cea392ee39920226172b193345536b344f0823c1
DougKeller <dwk24@zips.uakron.edu>
2020-07-12 17:02:19 -0400

    Remove debug logging

--- src/main/java/io/github/dougkeller/spigot_misc/mini_plugins/AutoCombine.java
+++ src/main/java/io/github/dougkeller/spigot_misc/mini_plugins/AutoCombine.java
@@ -3,7 +3,6 @@ package io.github.dougkeller.spigot_misc.mini_plugins;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityPickupItemEvent;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AutoCombine extends MiniPlugin {
@@ -21,26 +20,5 @@ public class AutoCombine extends MiniPlugin {
     }
 
     public void compressInventory(Player player) {
-        ItemStack[] itemStacks = player.getInventory().getStorageContents();
-        for (int i = 0; i < itemStacks.length; ++i) {
-            ItemStack itemStack = itemStacks[i];
-            String message;
-
-            if (itemStack == null) {
-                message = String.format(
-                        "[%d] Nothing",
-                        i
-                );
-            } else {
-                message = String.format(
-                        "[%d] %dx %s",
-                        i,
-                        itemStack.getAmount(),
-                        itemStack.getData().toString()
-                );
-            }
-
-            plugin.getLogger().info(message);
-        }
     }
 }
