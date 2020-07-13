5f63ec041e3683e1aef3bbf8f1ff9d5141a5385a
DougKeller <dwk24@zips.uakron.edu>
2020-07-12 22:55:45 -0400

    AutoCombine logging

--- src/main/java/io/github/dougkeller/spigot_misc/mini_plugins/AutoCombine.java
+++ src/main/java/io/github/dougkeller/spigot_misc/mini_plugins/AutoCombine.java
@@ -3,6 +3,8 @@ package io.github.dougkeller.spigot_misc.mini_plugins;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityPickupItemEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AutoCombine extends MiniPlugin {
@@ -20,5 +22,21 @@ public class AutoCombine extends MiniPlugin {
     }
 
     public void compressInventory(Player player) {
+        Inventory inventory = player.getInventory();
+        ItemStack[] itemStacks = inventory.getStorageContents();
+
+        for (int i = 0; i < itemStacks.length; ++i) {
+            ItemStack itemStack = itemStacks[i];
+            if (itemStack == null) {
+                continue;
+            }
+
+            String message = String.format(
+                    "[%d]: %s",
+                    i,
+                    itemStack.toString()
+            );
+            plugin.getLogger().info(message);
+        }
     }
 }
